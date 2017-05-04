e148c77faac852c74052b1393efdeba3de37e272
Yihui (Ray) Ren <yren2@vbi.vt.edu>
2017-05-04 10:41:44 -0400

    add more

--- macros.h
+++ macros.h
@@ -1,13 +1,58 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <limits>
+#include <iostream> 
+#include <iomanip>  
+#include <string>
+#include <sstream>  
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <cmath>
+#include <cassert>
+
+#define INF                         (int)1e9
+#define EPS                         1e-9
 
 #define bg     begin()
 #define pb     push_back
-#define all(c) c.begin(), c.end()
-#define forall(i,a,b) for(int i = a; i < b; i ++ )
+
+#define all(c)                      c.begin(), c.end()
+#define forall(i,a,b)               for(int i=a;i<b;i++)
+#define in(a,b)                     ( (b).find(a) != (b).end())
+
 using namespace std;
+
 typedef vector<int> vi;
 typedef pair<int,int> ii;
 typedef vector<vi>  vvi;
 typedef vector<ii>  vii;
 
+#ifdef DEBUG
+#define debug(args...)            {dbg,args; clog<<endl;}
+#define print_( a )               for( auto & x : a ) clog << x << ' '; clog << '\n';
+#define printPair_( a )           for( auto & x : a ) clog << '(' << x.first << ',' << x.second << ')' << ' '; clog << '\n';
+#else
+#define debug(args...)             // Just strip off all debug tokens
+#define print_( a )               // skip
+#define printPair_( a )           // skip
+#endif
+struct debugger
+{
+  template<typename T> debugger& operator , (const T& x)
+  {    
+    clog << x << " ";    
+    return *this;    
+  }
+} dbg;
 
+/******* Actual Code Starts Here *********/
+int main( int argc, char * argv[] ){
+  // std::ios::sync_with_stdio(false);
+
+  return 0;
+}
