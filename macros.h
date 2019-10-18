704679c19619777bfb23024a5d19674c912c87e3
Yihui (Ray) Ren <yren@bnl.gov>
2019-10-18 01:49:44 -0400

    add rolling hash

--- macros.h
+++ macros.h
@@ -65,7 +65,7 @@ void __f(const char* name, Arg1&& arg1){
 template <typename Arg1, typename... Args>
 void __f(const char* names, Arg1&& arg1, Args&&... args){
     const char* comma = strchr(names + 1, ',');
-    cerr.write(names, comma - names) << ": " << arg1 << " |";
+    cerr.write(names, comma - names) << ": " << arg1 << " | ";
     __f(comma + 1, args...);
 }
 
